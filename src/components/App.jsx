f4020626b8d0748de2d0f082381a2b024057bb63
Kristency <nandisubhajit666@gmail.com>
2019-06-02 11:59:43 +0530

    initial component scaffolding and setup

---
+++ src/components/App.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import StreamDelete from './streams/StreamDelete';
+import Header from './Header';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="ui container">
+				<BrowserRouter>
+					<Header />
+					<div>
+						<Route path="/" exact component={StreamList} />
+						<Route path="/streams/new" exact component={StreamCreate} />
+						<Route path="/streams/edit" exact component={StreamEdit} />
+						<Route path="/streams/delete" exact component={StreamDelete} />
+						<Route path="/streams/show" exact component={StreamShow} />
+					</div>
+				</BrowserRouter>
+			</div>
+		);
+	}
+}
+
+export default App;
